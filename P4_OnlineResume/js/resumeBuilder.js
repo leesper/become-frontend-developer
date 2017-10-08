180d62dfc921c5ff12e7e472a6adced7d265743f
leesper <pascal7718@gmail.com>
2017-10-08 10:30:48 +0800

    add work projects, education and bio

--- P4_OnlineResume/js/resumeBuilder.js
+++ P4_OnlineResume/js/resumeBuilder.js
@@ -2,40 +2,100 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
+var work = {
+  jobs: [
+    {
+      employer: 'M2MKey',
+      title: 'Golang Server Development Engineer',
+      location: 'Guiyang',
+      dates: '2016-now',
+      description: 'Server engineer for intelligent embedded devices'
+    },
+    {
+      employer: 'Electronic Soul',
+      title: 'Java Server Development Engineer',
+      location: 'Hang Zhou',
+      dates: '2015',
+      description: 'Server engineer for a MOBA mobile game'
+    },
+    {
+      employer: 'YY.com',
+      title: 'C++ Server Development Engineer',
+      location: 'Guang Zhou',
+      dates: '2013-2014',
+      description: 'Server engineer in YY Music Live'
+    }
+  ]
+};
+
+var projects = {
+  projects: [
+    {
+      title: 'TAO Framework',
+      dates: 'Apr 17, 2016 – Oct 8, 2017',
+      description: 'Light-weight TCP Asynchronous Golang Framework',
+      images: ''
+    },
+    {
+      title: 'go_rng',
+      dates: 'Mar 23, 2014 – Oct 8, 2017',
+      description: 'A pseudo-random number generator wriiten in Golang',
+      images: ''
+    },
+    {
+      title: 'CouchDB-Golang',
+      dates: 'Dec 4, 2016 – Oct 8, 2017',
+      description: 'A Golang library for working with CouchDB 2.x',
+      images: ''
+    },
+    {
+      title: 'Data Analyst Nanodegree',
+      dates: 'Mar 26, 2017 – Oct 8, 2017',
+      description: 'Udacity 数据分析纳米学位项目',
+      images: ''
+    },
+    {
+      title: 'holmes',
+      dates: 'Jul 3, 2016 – Oct 8, 2017',
+      description: 'Golang logging package',
+      images: ''
+    }
+  ]
+};
+
 var bio = {
   name: '李科君',
   role: '全栈工程师',
   contacts: {
+    blog: 'http://www.jianshu.com/u/10d26e0194c7',
     mobile: '18685118912',
     email: '394683518@qq.com',
-    twitter: 'http://weibo.com/u/3164852485/home',
     github: 'https://github.com/leesper',
-    blog: 'http://www.jianshu.com/u/10d26e0194c7'
+    twitter: 'http://weibo.com/u/3164852485/home',
+    location: 'City of Guiyang, Gui Zhou Province, China'
   },
   welcomeMessage: '一个很浪的全栈工程师',
   skills: ['Golang', 'Javascript', 'Python', 'R', 'Statistics'],
-  bioPic: 'images/fry.jpg'
+  biopic: 'images/fry.jpg'
 };
 
-var work = {}
-work.position = 'Server Development Engineer';
-work.employer = 'M2MKey';
-work.years = 2;
-work.city = 'Guiyang, Gui Zhou Province, China';
-
-var education = {}
-education['name'] = 'University of Science and Technology of China';
-education['years'] = "2010-2012";
-education['city'] = 'Hefei, An Hui Province, China';
-
-var formattedName = HTMLheaderName.replace('%data%', bio.name);
-var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
-var formattedBlog = HTMLblog.replace('%data%', bio.contacts.blog);
-var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-
-$('#header').prepend([formattedName, formattedRole, formattedMobile,
-  formattedEmail, formattedTwitter, formattedGithub, formattedBlog]);
+var education = {
+  schools: [
+    {
+      name: 'University of Science and Technology of China',
+      location: 'Hefei, An Hui Province, China',
+      degree: 'MA',
+      dates: '2006-2010',
+      url: 'http://ustc.edu.cn/',
+      majors: ['Software Engineering']
+    },
+    {
+      name: 'Sichuan University',
+      location: 'Chengdu, Si Chuan Province, China',
+      degree: 'BA',
+      dates: '2010-2013',
+      url: 'http://www.scu.edu.cn/',
+      majors: ['Information Security Engineering']
+    }
+  ]
+};
