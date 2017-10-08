89f0d9c43197f50c7414227cfff96b4a69885d82
leesper <pascal7718@gmail.com>
2017-10-08 09:50:17 +0800

    tomato commit

--- P4_OnlineResume/js/resumeBuilder.js
+++ P4_OnlineResume/js/resumeBuilder.js
@@ -2,8 +2,40 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$('#main').append(['李科君']);
+var bio = {
+  name: '李科君',
+  role: '全栈工程师',
+  contacts: {
+    mobile: '18685118912',
+    email: '394683518@qq.com',
+    twitter: 'http://weibo.com/u/3164852485/home',
+    github: 'https://github.com/leesper',
+    blog: 'http://www.jianshu.com/u/10d26e0194c7'
+  },
+  welcomeMessage: '一个很浪的全栈工程师',
+  skills: ['Golang', 'Javascript', 'Python', 'R', 'Statistics'],
+  bioPic: 'images/fry.jpg'
+};
 
-var awesomeThoughts = 'I am Leesper and I am AWESOME!';
+var work = {}
+work.position = 'Server Development Engineer';
+work.employer = 'M2MKey';
+work.years = 2;
+work.city = 'Guiyang, Gui Zhou Province, China';
 
-console.log(awesomeThoughts);
+var education = {}
+education['name'] = 'University of Science and Technology of China';
+education['years'] = "2010-2012";
+education['city'] = 'Hefei, An Hui Province, China';
+
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+var formattedBlog = HTMLblog.replace('%data%', bio.contacts.blog);
+var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+$('#header').prepend([formattedName, formattedRole, formattedMobile,
+  formattedEmail, formattedTwitter, formattedGithub, formattedBlog]);
