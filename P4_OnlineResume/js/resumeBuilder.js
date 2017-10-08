a9613364f48c8ec3399950e744604e8cbbdc9c58
leesper <pascal7718@gmail.com>
2017-10-08 08:09:42 +0800

    RING 1

--- P4_OnlineResume/js/resumeBuilder.js
+++ P4_OnlineResume/js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+$('#main').append(['李科君']);
+
+var awesomeThoughts = 'I am Leesper and I am AWESOME!';
+
+console.log(awesomeThoughts);
