911ef02e557ae92ffe90d9c598ba313b39a719f0
leesper <pascal7718@gmail.com>
2017-10-21 21:36:24 +0800

    finish writing Player class

--- P6_ArcadeGame/js/app.js
+++ P6_ArcadeGame/js/app.js
@@ -24,8 +24,7 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= canvas.width) {
       this.x = 0;
     }
-    // TODO: handle collision
-
+    this.checkCollisions(player);
 };
 
 Enemy.prototype.height = function() {
@@ -50,29 +49,77 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.dealWithCollision = function() {
-  player.reset();
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {};
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.reset();
+};
 
-Player.prototype.update = function() {};
-Player.prototype.render = function() {};
-Player.prototype.handleInput = function() {};
-Player.prototype.height = function() {};
-Player.prototype.width = function() {};
-Player.prototype.reset = function() {};
+Player.prototype.update = function() {
+  if (this.x >= canvas.width) {
+    this.x = canvas.width - this.width();
+  }
+
+  if (this.x <= 0) {
+    this.x = 0;
+  }
+
+  if (this.y >= canvas.height) {
+    this.y = canvas.height - this.height();
+  }
+
+  if (this.y <= 0) {
+    this.y = 0;
+  }
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode) {
+  switch (keyCode) {
+    case 'left':
+      this.x -= this.width();
+      break;
+    case 'up':
+      this.y -= this.height();
+      break;
+    case 'right':
+      this.x += this.width();
+      break;
+    case 'down':
+      this.y += this.height();
+      break;
+  }
+};
+
+Player.prototype.height = function() {
+  return Resources.get(this.sprite).height;
+};
+
+Player.prototype.width = function() {
+  return Resources.get(this.sprite).width;
+};
+
+Player.prototype.reset = function() {
+  this.x = canvas.width / 2;
+  this.y = canvas.height - 40;
+};
 
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+for (var i = 0; i < 10; i++) {
+  allEnemies.push(new Enemy());
+}
+
 // Place the player object in a variable called player
-
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
