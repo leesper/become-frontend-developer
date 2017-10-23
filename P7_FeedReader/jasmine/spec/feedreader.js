270bda2cbd6b1c6027b256a53ded984c6c686115
leesper <pascal7718@gmail.com>
2017-10-23 16:49:34 +0800

    RSS Feeds suite finished

--- P7_FeedReader/jasmine/spec/feedreader.js
+++ P7_FeedReader/jasmine/spec/feedreader.js
@@ -27,16 +27,30 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL is defined and not empty', function() {
+          allFeeds.forEach(function(feed) {
+            expect(feed.url).toBeDefined();
+            expect(feed.url).not.toBe('');
+            expect(feed.url).not.toBeNull();
+          });
+        });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name is defined and not empty', function() {
+          allFeeds.forEach(function(feed) {
+            expect(feed.name).toBeDefined();
+            expect(feed.name).not.toBe('');
+            expect(feed.name).not.toBeNull();
+          });
+        });
     });
 
 
