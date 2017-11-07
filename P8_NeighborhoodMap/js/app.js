2b56db2e6ce6900f1a074a6fa40e80f30bca98c0
leesper <pascal7718@gmail.com>
2017-11-07 23:05:05 +0800

    refactor view model

--- P8_NeighborhoodMap/js/app.js
+++ P8_NeighborhoodMap/js/app.js
@@ -1,21 +1,69 @@
 const AppViewModel = function() {
   this.locations = ko.observableArray([
-    '灵隐寺',
-    '北高峰',
-    '云溪竹径',
-    '钱江一桥',
-    '断桥残雪',
-    '台塑牛排',
-    '千串屋',
-    '柳浪闻莺',
-    '南宋御街',
-    '五云山',
-    '梅家坞',
-    '外婆家',
-    '六和塔',
-    '曲院风荷',
-    '玉皇山',
-    '楼外楼'
+    {
+      name: '灵隐寺',
+      visible: true,
+    },
+    {
+      name: '北高峰',
+      visible: true,
+    },
+    {
+      name: '云溪竹径',
+      visible: true,
+    },
+    {
+      name: '钱江一桥',
+      visible: true,
+    },
+    {
+      name: '断桥残雪',
+      visible: true,
+    },
+    {
+      name: '台塑牛排',
+      visible: true,
+    },
+    {
+      name: '千串屋',
+      visible: true,
+    },
+    {
+      name: '柳浪闻莺',
+      visible: true,
+    },
+    {
+      name: '南宋御街',
+      visible: true,
+    },
+    {
+      name: '五云山',
+      visible: true,
+    },
+    {
+      name: '梅家坞',
+      visible: true,
+    },
+    {
+      name: '满觉陇',
+      visible: true,
+    },
+    {
+      name: '六和塔',
+      visible: true,
+    },
+    {
+      name: '曲院风荷',
+      visible: true,
+    },
+    {
+      name: '玉皇山',
+      visible: true,
+    },
+    {
+      name: '楼外楼',
+      visible: true,
+    }
   ]);
 };
 
