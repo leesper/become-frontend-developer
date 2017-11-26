6a6b351cceb53cc60ad0808cd0abf85858b2eff9
leesper <pascal7718@gmail.com>
2017-11-26 23:08:22 +0800

    fix bug

--- P9_MyReads/src/BookItem.js
+++ P9_MyReads/src/BookItem.js
@@ -8,7 +8,7 @@ class BookItem extends React.Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.image})` }} ></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.image})` }} ></div>
             <Changer
               book={this.props.book}
               shelf={this.props.shelf}
